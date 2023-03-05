235708ab2841cd7034d6c60439689e7c97c23fd8
Elinor Chayo <elliechayo@gmail.com>
2023-03-05 18:42:06 -0500

    added js file

---
+++ script.js
@@ -0,0 +1,13 @@
+const cardsUl = document.getElementById("cardsUl");
+
+function showCardsUl() {
+  cardsUl.classList.remove("hidden");
+  cardsUl.classList.add("block");
+}
+
+function hideCardsUl() {
+  cardsUl.classList.add("hidden");
+  cardsUl.classList.remove("block");
+}
+
+showCardsUl();
