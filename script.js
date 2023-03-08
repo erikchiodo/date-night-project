c057870f234385c9cbc84b8cd9ea0debb7c3d455
erikchiodo <erikchiodo@gmail.com>
2023-03-07 19:02:41 -0500

    Added javascript code for Google Maps and Geocoding Apis

--- script.js
+++ script.js
@@ -12,4 +12,50 @@ function hideCardsUl() {
 
 showCardsUl();
 
-console.log(cardsUl);
+const searchInput = document.querySelector("#searchInput");
+const searchBtn = document.querySelector("#search-btn");
+const cardUl = document.querySelector("#cardsUl");
+
+searchBtn.onclick = function () {
+  const zipCode = searchInput.value;
+  if (zipCode) {
+    convertZipToGeo(zipCode);
+    // getRestaurants(zipCode);
+  }
+};
+
+var convertZipToGeo = function(zipCode) {
+  var geoCodeURL = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCmdB_Ov1gxNFIiPerrkI8HQZ3j018SxF8&libraries=places&callback=initMap&address=" + zipCode
+
+  fetch(geoCodeURL).then(function(response)) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        
+      }
+    }
+  }
+}
+
+
+var getRestaurants = function (zipCode) {
+  // Add zip code to googleMapsUrl and as parameter for getRestaurants function
+  var googleMapsUrl =
+    "https://maps.googleapis.com/maps/api/js?key=AIzaSyCmdB_Ov1gxNFIiPerrkI8HQZ3j018SxF8&libraries=places&callback=initMap&address=06853";
+
+  fetch(googleMapsUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRestaurants(data);
+      });
+    } else {
+      alert("Error! Check API Get Call");
+    }
+  });
+};
+
+var displayRestaurants(restaurants) {
+  if (restaurants.length == 0) {
+    cardUl.textContent = "No Restaurants available in your area!"
+      return;
+  }
+}
