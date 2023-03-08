8480490335ac5fb0e44f1788244a0f989fbc73b5
erikchiodo <erikchiodo@gmail.com>
2023-03-08 18:59:13 -0500

    Updated JavaScript (fixed bugs)

--- script.js
+++ script.js
@@ -12,50 +12,67 @@ function hideCardsUl() {
 
 showCardsUl();
 
-const searchInput = document.querySelector("#searchInput");
 const searchBtn = document.querySelector("#search-btn");
+const searchInput = document.querySelector("#searchInput");
 const cardUl = document.querySelector("#cardsUl");
 
-searchBtn.onclick = function () {
-  const zipCode = searchInput.value;
+searchBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  var zipCode = searchInput.value;
+  // console.log(zipCode);
   if (zipCode) {
     convertZipToGeo(zipCode);
     // getRestaurants(zipCode);
+  } else if (zipCode == "") {
+    alert("Please submit zip code");
+  } else {
+    alert("Please re-submit zip code");
   }
+});
+
+// Function is inputting zipCode (collected from user input) and converting into location (lat and long codes). Passed to getRestaurant function
+var convertZipToGeo = function (zipCode) {
+  var geoCodeURL =
+    "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCmdB_Ov1gxNFIiPerrkI8HQZ3j018SxF8&libraries=places&callback=initMap&address=" +
+    zipCode;
+  fetch(geoCodeURL)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert("Wrong Zip Code");
+      }
+    })
+    .then(function (data) {
+      console.log(this);
+      const geoLocation = data.location;
+      getRestaurants(geoLocation);
+    });
 };
 
-var convertZipToGeo = function(zipCode) {
-  var geoCodeURL = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCmdB_Ov1gxNFIiPerrkI8HQZ3j018SxF8&libraries=places&callback=initMap&address=" + zipCode
-
-  fetch(geoCodeURL).then(function(response)) {
-    if (response.ok) {
-      response.json().then(function(data) {
-        
-      }
-    }
-  }
-}
-
-
-var getRestaurants = function (zipCode) {
+// Function is inputting lat-long code into fetch call to google maps. Passed to displayRestaurants function.
+var getRestaurants = function (latLong) {
   // Add zip code to googleMapsUrl and as parameter for getRestaurants function
   var googleMapsUrl =
-    "https://maps.googleapis.com/maps/api/js?key=AIzaSyCmdB_Ov1gxNFIiPerrkI8HQZ3j018SxF8&libraries=places&callback=initMap&address=06853";
+    // Need to figure out how to parse location (latLong) variable into lat and long and replace location within below url
+    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=20&type=restaurant&key=AIzaSyCmdB_Ov1gxNFIiPerrkI8HQZ3j018SxF8";
 
-  fetch(googleMapsUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRestaurants(data);
-      });
-    } else {
-      alert("Error! Check API Get Call");
-    }
-  });
+  fetch(googleMapsUrl)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert("Error! Check API Get Call");
+      }
+    })
+    .then(function (data) {
+      displayRestaurants(data);
+    });
 };
 
-var displayRestaurants(restaurants) {
+var displayRestaurants = function (restaurants) {
   if (restaurants.length == 0) {
-    cardUl.textContent = "No Restaurants available in your area!"
-      return;
+    cardUl.textContent = "No Restaurants available in your area!";
+    return;
   }
-}
+};
